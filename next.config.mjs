3a0f2e6f6fb12c4cb47ac036da1ef98fee654f3b
Shahab155 <bhuttoshahab1544@gmail.com>
2025-12-25 22:14:41 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.tacdn.com',
+        pathname: '/media/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
